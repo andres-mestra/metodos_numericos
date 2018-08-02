91143faea4e0d46b1c2dba5dd2c45ef9db0876d7
Andres Mestra <andresmestra11a@gmail.com>
2018-08-02 05:40:06 -0500

    correccion error de  save

--- program01.py
+++ program01.py
@@ -1,8 +1,9 @@
-import numpy as np 
+import numpy as np
+import  matplotlib.pyplot as plt
 
 def factorial(n):
     if n == 0:
-        print 1
+        return 1
     else:
         fat = 1
         for i in range(1, n+1):
@@ -11,15 +12,39 @@ def factorial(n):
         return  fat 
         
 
-print(factorial(3))
+print(factorial(5))
+
 
 def cos(x,n):
     resul  = 0
     for i in range(0,n+1):
-        resul += (-1**i)*x**(2*i+1)/factorial(2*i+1)
+        resul += (-1)**i * x**(2*i)/factorial(2*i)
     return resul
 
+"""
 x = int(raw_input("The value of x:"))
 n = int(raw_input("The value of n:"))
 print(cos(x,n))
+"""
+
+Npoints = 100
+dtheta = (2.0*np.pi -0.0)/Npoints
+Nterm = 10
+
+theta = []
+ctheta = []
+
+for i in range(0, Npoints):
+    theta.append(i*dtheta)
+    ctheta.append(cos(i*dtheta, Nterm ))
+
+fig = plt.figure()
+ax= plt.axes()
+ax.plot(theta,ctheta, '.')
+ax.plot(theta,np.cos(theta))
+ax.grid()
+ax.set_xlabel(r"$\theta$", fontsize=14)
+plt.show()
+plt.savefig('cos.png')
+
 
