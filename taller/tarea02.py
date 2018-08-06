7bd0cb086a42dab3b7f554a74cac5993f7e5a2a2
Andres Mestra <andresmestra11a@gmail.com>
2018-08-06 09:52:56 -0500

    tarea02, presición

---
+++ taller/tarea02.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+"""
+[[a, b], *[x,y] = [e,f]
+ [c, d]]
+"""
+def resolverSist(a,b,c,d,e,f):
+    if a != 0:
+        try:
+            m = c/a
+            d1 = d - m*b
+            f1 = f - m*e
+            y = f1/d1
+            x = ( e - b*y) / a
+            print("16bits: x =" + str(x) +" , y =" + str(y))
+        except Exception as err:
+            print("Error:" + str(err)) 
+    else:
+        print("a es cero o indefinida")
+
+#1.A
+resolverSist(np.float16(1.130),
+             np.float16(6.990),
+             np.float16(1.013),
+             np.float16(6.099),
+             np.float16(14.20),
+             np.float16(4.22))
+#1.B
+
+
+
