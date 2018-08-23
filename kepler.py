9daa7b3d236fd48924ad4cc040e410d26fa9e2d4
Andres Mestra <andresmestra11a@gmail.com>
2018-08-23 10:34:36 -0500

    Correción en el calculo de phi, grafica en cordenadas cartesianas

--- kepler.py
+++ kepler.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 G = 6.67e-11
 Ms = 1.989e30
@@ -37,7 +38,6 @@ for i in range(0, len(time_array)):
         xcurt = xpre - f(xpre,time_array[i])/dfdE(xpre)
         error = abs(xpre - xcurt)
         iteracion += 1
-        
         #print("xpre=%f\t xcurt=%f\t f(xcurt)=% f\t error=%f"%(xpre,xcurt,f(xcurt,time_array[i] ),error))
         xpre = xcurt
 
@@ -45,12 +45,30 @@ for i in range(0, len(time_array)):
             print("Numero de iteraciones  excedido")
             break
 
-    phi = 2.0*np.arctan((1+e)/(1-e))**0.5 * np.tan(0.5*xcurt)
+    phi = 2.0*np.arctan(((1+e)/(1-e))**0.5 * np.tan(0.5*xcurt))
     r = a*(1-e**2)/(1+e*np.cos(phi))
-
+   
     phi_array.append(phi)
     r_array.append(r)
 
-
+"""
 for i in range(0, len(phi_array)):
     print("Phi: %f, r: %f"%(phi_array[i],r_array[i]))
+"""
+
+
+x_array = []
+y_array = []
+
+for i in range(0, len(r_array)):
+    x_array.append(r_array[i]*np.cos(phi_array[i])/AU)
+    y_array.append(r_array[i]*np.sin(phi_array[i])/AU)
+
+
+fig = plt.figure()
+ax = plt.axes()
+ax.set_xlabel("X (AU)")
+ax.set_ylabel("y (AU)")
+ax.plot(x_array, y_array)
+ax.plot(0,0,'*')#posición del sol respecto a la orbina del cometa Halley
+plt.show()
